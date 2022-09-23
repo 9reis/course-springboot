e902a6372dbf89cb198f48c6d94eaed95e72ac36
Lucas Reis <lucasreis_9@hotmail.com>
2022-09-23 12:15:09 -0300

    Subtotal & Total methods

--- course/src/main/java/com/example/demo/entities/OrderItem.java
+++ course/src/main/java/com/example/demo/entities/OrderItem.java
@@ -64,6 +64,11 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
